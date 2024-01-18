9899b093e76df6c4db7ca594e88181cc25a7ba20
Gabriele02 <gagag@g,ail.com>
2024-01-18 17:26:29 +0100

    Added button for delete ingredient

--- src/main/java/org/learning/la/mia/pizzeria/crud/controller/IngredientController.java
+++ src/main/java/org/learning/la/mia/pizzeria/crud/controller/IngredientController.java
@@ -3,6 +3,7 @@ package org.learning.la.mia.pizzeria.crud.controller;
 import jakarta.validation.Valid;
 import org.learning.la.mia.pizzeria.crud.interfaccie.IngredientsRepository;
 import org.learning.la.mia.pizzeria.crud.model.Ingredient;
+import org.learning.la.mia.pizzeria.crud.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -49,6 +51,18 @@ public class IngredientController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ingredient with id " + id + " not found");
 
+        }
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes){
+        Optional<Ingredient> result = ingredientsRepository.findById(id);
+        if (result.isPresent()){
+            ingredientsRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("redirectMessage","ingredient" + result.get().getName() + "deleted!");
+            return "redirect:/ingredient";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
         }
     }
 }
